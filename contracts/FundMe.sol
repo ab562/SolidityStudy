651e93bb903d0d85dbd0199cd8fdd1886b0abeb2
xiaokangy <xiaokangy@gmail.com>
2025-09-25 17:56:08 +0800

    init

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,6 +8,14 @@ pragma solidity >=0.8.2 <0.9.0;
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
 contract FundMe {
-    
+    mapping(address=>uint) public fundersToAmount;
+    uint256 min_value=1*10**18;
+    //external 外部调用
+    // payable 收款 eth
+    function fund() external payable {
+        // 如果条件不满足则回退交易
+        require(msg.value>=min_value,"send more eth");
+        fundersToAmount[msg.sender]=msg.value;
+    }
      
 }
