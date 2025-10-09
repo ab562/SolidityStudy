631b5c3af349c7d051360d46c6069a2e44b8eada
ab562 <6a.b5-62@163.com>
2025-10-09 21:09:21 +0800

    时间锁

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -15,13 +15,18 @@ contract FundMe {
     uint256 min_value=1*10**18;
     uint256 constant TARGET =10*10**18;
     AggregatorV3Interface internal dataFeed;
+    uint256 deploymentTimestamp;
+    // 锁定时长
+    uint256 lockTime;
     address public owner;
     
-    constructor() {
+    constructor(uint256  _lockTime) {
         dataFeed = AggregatorV3Interface(
             0x694AA1769357215DE4FAC081bf1f309aDC325306
         );
         owner=msg.sender;
+        deploymentTimestamp=block.timestamp;
+        lockTime=_lockTime;
     }
 
     //external 外部调用
@@ -29,6 +34,7 @@ contract FundMe {
     function fund() external payable {
         // 如果条件不满足则回退交易
         require(coverEthToUsd(msg.value) >=min_value,"send more eth");
+         require(block.timestamp < deploymentTimestamp+lockTime,"window is closed");
         fundersToAmount[msg.sender]=msg.value;
     }
 
@@ -47,23 +53,33 @@ contract FundMe {
         uint256 ethPrice = uint256(getChainlinkDataFeedLatestAnswer());
         return ethPrice * ethAmount/(10**8);
     }
-    function transferOwnership(address newOwner) public {
-        require(msg.sender==owner,"this function con only be called by owner");
+    function transferOwnership(address newOwner) public onluOwner {
+    
         owner=newOwner;
     }
 
-    function getFund() external {
+    function getFund() external windowClose onluOwner {
+       
         // 当前合约地址
        require( coverEthToUsd(address(this).balance) >=TARGET,"Target is not reached");
-       require(msg.sender==owner,"this function con only be called by owner");
+      
        payable(msg.sender).transfer(address(this).balance);
     }
 
-    function refund() external  {
+    function refund() external windowClose {
         require( coverEthToUsd(address(this).balance) < TARGET,"Target is  reached");
         uint256 amount= fundersToAmount[msg.sender];
         require( amount==0,"There is no fund for you");
         payable(msg.sender).transfer(amount);
         fundersToAmount[msg.sender]=0;
+    }
+    modifier windowClose (){
+         require(block.timestamp >deploymentTimestamp+lockTime,"window is no closed");
+         _;
+    }
+
+       modifier onluOwner (){
+         require(msg.sender==owner,"this function con only be called by owner");
+         _;
     }
 }
