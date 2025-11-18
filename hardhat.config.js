90750174798a26f19979e207b47bf6776dc5b722
tom <a.b5-62@163.com>
2025-11-18 22:06:15 +0800

    2222333

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "sepolia",
   solidity: "0.8.28",
+
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/dcRZuzms0ttyvNGeEN1UE",
+      accounts: ["0x6e4a6fcf316d086d48e4cdc1e034d3bbecf13459cdfc31dd6391660541beac59","0x8bb2e366f56e5b867099e525443430c4b1f146c59df223fa4e04fb1194f6271b"],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: "dcRZuzms0ttyvNGeEN1UE",
+    }
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,36 @@
 
-const { ethers } = require("hardhat");
+const hre = require("hardhat");
+const { ethers } = hre;
 
 async function main() {
     const FundMe = await ethers.getContractFactory("FundMe");
-    const fundMe = await FundMe.deploy(10);
+    const fundMe = await FundMe.deploy(100);
     await fundMe.waitForDeployment();
+    await fundMe.deploymentTransaction().wait(2);
+    await verifyFundMe(fundMe.target, [100])
     console.log("FundMe deployed to:", fundMe.target);
+    const [a1,a2] = await ethers.getSigners();
+    const fundtx = await fundMe.fund({value: ethers.parseEther("0.01")});
+    await fundtx.wait();
+    let bl=await ethers.provider.getBalance(fundMe.target);
+    console.log("Balance of fundMe:", bl);
+
+    const fundtx2 = await fundMe.connect(a2).fund({value: ethers.parseEther("0.02")});
+    await fundtx2.wait();
+    bl=await ethers.provider.getBalance(fundMe.target);
+    console.log("Balance of fundMe:", bl);
+
+    const a1b = await fundMe.fundersToAmount(a1.address);
+    const a2b = await fundMe.fundersToAmount(a2.address);
+    console.log("Balance of a1:", a1b);
+    console.log("Balance of a2:", a2b);
+}
+
+async function verifyFundMe(fundMeAddr, args) {
+    await hre.run("verify:verify", {
+        address: fundMeAddr,
+        constructorArguments: args,
+      });
 }
 
 main().catch((error)=>{
